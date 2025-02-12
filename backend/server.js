b6ecde3e3677060435d2242fc5ba765b76c392c8
Paws <131801287+paws1234@users.noreply.github.com>
2025-02-13 03:16:40 +0800

    Update server.js
    trial and error 10

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,9 @@ app.use(
 );
 
 const imageUrlPattern =
-  /^https:\/\/xcimg\.szwego\.com\/\d{8}\/([ai])\d+_\d+(?:_\d+)?\.jpg\?imageMogr2\/.*$/;
+//  /^https:\/\/xcimg\.szwego\.com\/\d{8}\/([ai])\d+_\d+(?:_\d+)?\.jpg\?imageMogr2\/.*$/;
+    /^https:\/\/xcimg\.szwego\.com\/\d{8}\/([ai])\d+_\d+(?:_\d+)?\.jpg/;
+
 
 const MAX_TABS = 5;
 const WAIT_TIME = 200000;
